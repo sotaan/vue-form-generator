521b5aa4816bdcb15661e63bd62f370aa69b0c9f
David Mamane <davidmamane@hellomojito.com>
2016-09-07 11:12:29 +0200

    hint on fields

--- src/formGenerator.vue
+++ src/formGenerator.vue
@@ -1,10 +1,12 @@
 <template>
 	<fieldset v-if="schema != null">
 		<div class="col-md-6 col-sm-6 col-xs-12">
-			<div v-for="field in fields" v-if="fieldVisible(field)" :class="getFieldRowClasses(field)" class="form-group"><span v-if="field.help" class="help">
-		      <div class="helpText">{{{field.help}}}</div></span>
+			<div v-for="field in fields" v-if="fieldVisible(field)" :class="getFieldRowClasses(field)" class="form-group">
+				<span v-if="field.help" class="help">
+		      <div class="helpText">{{{field.help}}}</div>
+				</span>
 		    <div :class="getFieldWrapClasses(field)">
-		      <component :is="getFieldType(field)" :disabled="fieldDisabled(field)" :model.sync="model" :schema.sync="field"></component>
+		      <component :is="getFieldType(field)" :disabled="fieldDisabled(field)" :model.sync="model" :schema.sync="field" @focus="console.log('je teste mon evenement')"></component>
 		      <label v-if="isClassicLabel(field)" data-content="{{field.label}}">{{ field.label }}</label>
 		      <div v-if="field.buttons &amp;&amp; field.buttons.length &gt; 0" class="buttons">
 		        <button v-for="btn in field.buttons" @click="btn.onclick(model, field)" :class="btn.classes" class="btn btn-default">{{ btn.label }}</button>
