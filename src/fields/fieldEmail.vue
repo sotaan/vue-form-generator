48c5e023b06be087f9f956fab41a8e8e67e3bde1
David Mamane <davidmamane@hellomojito.com>
2016-12-20 02:03:21 +0100

    e-mail layout & datetime removed blur event

--- src/fields/fieldEmail.vue
+++ src/fields/fieldEmail.vue
@@ -1,7 +1,8 @@
 <template lang="jade">
-.material-textfield.grey
-	input(type="email", v-model="value", :readonly="schema.readonly", :disabled="disabled", :placeholder="schema.placeholder")
-	label(data-content="{{schema.label}}") {{ schema.label }}
+	span.input
+    input.input__field(type="text", v-model="value", :maxlength="schema.max", :readonly="schema.readonly", :disabled="disabled", :placeholder="schema.placeholder", @focus="toggleHelp", @blur="toggleHelp")
+    label.input__label(for='input')
+      span.input__label-content {{ schema.label }}
 </template>
 
 <script>
